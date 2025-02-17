f5c92b3812c2f16a4668602587bc94ac8e4b2e64
pgcs22 <pgcs_22@hotmail.com>
2025-02-17 14:24:14 -0300

    construção da tabela na página 2 com scrollbar transparente

--- pages/pagina2.py
+++ pages/pagina2.py
@@ -1,4 +1,4 @@
-from dash import html, dcc, callback, Input, Output
+from dash import html, dcc, callback, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import numpy as np
@@ -8,24 +8,23 @@ from dash import html, dcc
 
 dataframe = pd.read_excel("banco de dados.xlsx", sheet_name="Banco de Dados")
 dataframe = dataframe.dropna(axis=1, how='all')
-dataframe['Movimentação']=dataframe['Depósito']-dataframe['Retiradas']
+dataframe['Movimentação'] = dataframe['Depósito']-dataframe['Retiradas']
 ultimo_mes = dataframe['Data'].max()
 
 df_1 = dataframe.sort_values(by=['Investimento', 'Data'])
 
-df_1['rendimento_acumulado'] = df_1.groupby('Investimento')['Rendimento']\
+df_1['Acumulado 12 Meses'] = df_1.groupby('Investimento')['Rendimento']\
     .rolling(window=12, min_periods=1).apply(lambda x: (1 + x).prod() - 1, raw=True)\
     .reset_index(level=0, drop=True)
 
-df_1['rendimento_acumulado'] = df_1.groupby('Investimento')['rendimento_acumulado'].shift(1)
+df_1['Acumulado 12 Meses'] = df_1.groupby('Investimento')['Acumulado 12 Meses'].shift(1)
 
 df_acumulado = df_1[df_1['Data'] == ultimo_mes]
-df_acumulado = df_acumulado.sort_values(by=['rendimento_acumulado'])
-
-print(df_acumulado[['Banco', 'Investimento', 'rendimento_acumulado']])
+df_acumulado = df_acumulado.sort_values(by=['Acumulado 12 Meses'], ascending=[False])
+df_12meses = df_acumulado[['Banco', 'Investimento', 'Acumulado 12 Meses']]
 
 layout = dbc.Container(
-fluid=True,  # Ocupa toda a largura disponível
+    fluid=True,  # Ocupa toda a largura disponível
     style={
         'overflowX': 'auto',  # Habilita a barra de rolagem horizontal
         'width': '100%',  # Largura total
@@ -34,13 +33,44 @@ fluid=True,  # Ocupa toda a largura disponível
     children=[dbc.Row(
         children=[dbc.Col(
             children=[dbc.Card(
-                children=[dbc.CardHeader('', className='paragrafo-personalizado'),
-                          dbc.CardBody()
+                children=[dbc.CardHeader('Rendimento acumulado em 12 Meses', className='paragrafo-personalizado'),
+                          dbc.CardBody(
+                             dash_table.DataTable(
+                                 id='Tabela1',
+                                 columns=[
+                                     {'name': 'Banco', 'id': 'Banco'},
+                                     {'name': 'Investimento', 'id': 'Investimento'},
+                                     {"name": "Acumulado 12 Meses", "id": "Acumulado 12 Meses",
+                                      "type": "numeric", "format": {"specifier": ".2%"}}
+                                 ],
+                                 data=df_12meses.to_dict('records'),
+                                 style_table={'overflowX': 'auto', 'overflowY': 'auto', 'textAlign': 'left'},
+                                 style_cell={
+                                     'textAlign': 'left',  # Alinhamento do texto
+                                     'padding': '10px',  # Espaçamento interno
+                                     'color': 'white',  # Cor da fonte
+                                     'fontFamily': 'Candal',  # Fonte do texto
+                                     'fontSize': '14px',  # Tamanho da fonte
+                                     'textAlign': 'center',
+                                     'backgroundColor': 'Transparent'
+                                 },
+                                 style_header={'backgroundColor': 'Transparent',
+                                               'fontWeight': 'bold',
+                                               'textAlign': 'center',
+                                               'color': 'white'},
+                                 style_as_list_view=True
+                             )
+                          )
                           ],
                 color="transparent",  # Cor de fundo
                 inverse=True,  # Texto branco
                 outline=False,  # Sem borda
-            )],width=6, lg=3),
+                style={
+                    'max-height': '50vh',  # Define a altura máxima do card
+                    'overflow': 'auto',
+
+                }
+            )], width=9, lg=4, md=4, sm=9),
             dbc.Col(
                 children=[dbc.Card(
                     children=[dbc.CardHeader('', className='paragrafo-personalizado'),
@@ -49,7 +79,7 @@ fluid=True,  # Ocupa toda a largura disponível
                     color="transparent",  # Cor de fundo
                     inverse=True,  # Texto branco
                     outline=False,  # Sem borda
-                )],width=6, lg=3),
+                )], width=6, lg=3),
             dbc.Col(
                 children=[dbc.Card(
                     children=[dbc.CardHeader('', className='paragrafo-personalizado'),
@@ -58,15 +88,7 @@ fluid=True,  # Ocupa toda a largura disponível
                     color="transparent",  # Cor de fundo
                     inverse=True,  # Texto branco
                     outline=False,  # Sem borda
-                )],width=6, lg=3),
-            dbc.Col(children=[dbc.Card(
-                    children=[dbc.CardHeader('', className='paragrafo-personalizado'),
-                          dbc.CardBody()
-                          ],
-                    color="transparent",  # Cor de fundo
-                    inverse=True,  # Texto branco
-                    outline=False,  # Sem borda
-                )],width=6, lg=3)
+                )], width=6, lg=3),
         ], style={"margin-bottom": "20px"}  # Adiciona margem abaixo da linha
     ),
         dbc.Row(children=[
